3c2ea3706df32e19578feae6fb1a1ddf9c937bce
bertrandmoal <bertrandmoal@gmail.com>
2021-02-25 18:07:06 +0000

    correction data

---
+++ FourCePhase2.1ards/R/data.R
@@ -0,0 +1,14 @@
+#ICD10 code regroups for complication analysis
+"comp_lass"
+
+#lab_mapping with nam and loinc
+"lab_mapping"
+
+#medication class
+"med_code"
+
+#link between ICD and pheno
+"pheno_ICD"
+
+#severe procedure for 4CE severe definition with PROC-ICD10 classification
+"sev_proc_icd10"
