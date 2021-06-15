0c5b5a4bad8123e79240d85f494d4e623811e0d5
bertrandmoal <bertrandmoal@gmail.com>
2021-06-15 14:57:05 +0000

    correction other sex

--- FourCePhase2.1ards/R/runAnalysis.R
+++ FourCePhase2.1ards/R/runAnalysis.R
@@ -1919,7 +1919,7 @@ runAnalysis <- function() {
         dplyr::mutate(std_value = ifelse(npat == 1, 0, std_value),
                       std_log_value = ifelse(npat == 1, 0, std_log_value))
 
-    var_gen_num = c("number","n_severe","npat_nodiag","never_severe","ever_severe","26to49","50to69","70to79","80plus","18to25","female","male","no_previous_hospi","previous_hospi","no_rehospit","rehospit","dead","dead_less_28","dead_less_90","out_hospit_28","out_hospit_90","in_hospital", "death", "out_hospital","no_data")
+    var_gen_num = c("number","n_severe","npat_nodiag","never_severe","ever_severe","26to49","50to69","70to79","80plus","18to25","female","male","other","no_previous_hospi","previous_hospi","no_rehospit","rehospit","dead","dead_less_28","dead_less_90","out_hospit_28","out_hospit_90","in_hospital", "death", "out_hospital","no_data")
 
     var_gen_ag =c("mean_freq","std_freq", "mean_los", "std_los")
 
